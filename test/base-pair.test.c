4e67c43a017d42507bfb4db9d091be5f84503fa2
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-03-20 10:17:37 +0000

    test: non-matching base DNA pairs

--- test/base-pair.test.c
+++ test/base-pair.test.c
@@ -17,6 +17,34 @@ START_TEST(matching_base_pair)
 }
 END_TEST
 
+START_TEST(non_matching_base_pair_A)
+{
+  // arrange
+  char l = 'A';
+  char r = 'A';
+
+  // act
+  bool matching = matchingBasePair(l, r);
+
+  // assert
+  fail_unless(matching == false);
+}
+END_TEST
+
+START_TEST(non_matching_base_pair_B)
+{
+  // arrange
+  char l = 'C';
+  char r = 'T';
+
+  // act
+  bool matching = matchingBasePair(l, r);
+
+  // assert
+  fail_unless(matching == false);
+}
+END_TEST
+
 Suite *base_pair_suite(void) {
   Suite *s;
   TCase *tc_core;
@@ -25,6 +53,8 @@ Suite *base_pair_suite(void) {
   tc_core = tcase_create("Core");
 
   tcase_add_test(tc_core, matching_base_pair);
+  tcase_add_test(tc_core, non_matching_base_pair_A);
+  tcase_add_test(tc_core, non_matching_base_pair_B);
   suite_add_tcase(s, tc_core);
 
   return s;
