79a5742fb3e90e1e67c6396dc2f0a034a4a1eff5
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-03-25 20:26:35 +0000

    feat: add covid19 genome

---
+++ src/covid-19.h
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+
+#ifndef COVID_19
+#define COVID_19
+
+extern const char *COVID_19_GENOME;
+
+#endif /* COVID_19 */
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "./base-pair.h"
+#include "./covid-19.h"
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -7,21 +8,9 @@
  *  Since we're collecting sequences from the user we'll limit
  *  this to something manageable.
  */
-const int MAX_SEQ_LEN = 100;
-
 int main() {
-  char l[MAX_SEQ_LEN];
-  char r[MAX_SEQ_LEN];
 
-  printf("Type the first base pair sequence to compare:\n");
-  fgets(l, MAX_SEQ_LEN, stdin);
-
-  printf("Type the second base pair sequence to compare:\n");
-  fgets(r, MAX_SEQ_LEN, stdin);
-
-  bool matching = matchingSequences(l, r);
-  printf(matching == false ? "The genome sequences do not form a pair\n"
-                           : "The genome sequence does form a pair\n");
+  printf("covid-19-genome:\n%s\n", COVID_19_GENOME);
 
   return 0;
 }
