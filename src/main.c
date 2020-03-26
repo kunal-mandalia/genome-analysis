a933493975d75543202cf06954889952727ea3d4
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-03-26 09:44:12 +0000

    chore: remove comment

--- src/main.c
+++ src/main.c
@@ -3,14 +3,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-/**
- *  The human genome contains around 3 billion base pairs.
- *  Since we're collecting sequences from the user we'll limit
- *  this to something manageable.
- */
 int main() {
-
   printf("covid-19-genome:\n%s\n", COVID_19_GENOME);
-
   return 0;
 }
